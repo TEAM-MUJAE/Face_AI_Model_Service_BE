7e5a1276395a2f085bbb4f39ec145bbe4b6314f0
zzimmi <miryeong0812@naver.com>
2024-03-14 16:47:34 +0900

    20240314 mapper, DTO 수정

---
+++ src/main/java/com/mujae/member/dto/MemberDTO.java
@@ -0,0 +1,93 @@
+package com.mujae.member.dto;
+
+import org.apache.ibatis.type.Alias;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.web.context.WebApplicationContext;
+
+
+
+@Alias("mdto")
+public class MemberDTO {
+	private int user_code;
+	private String id;
+	private String pwd;
+	private String name;
+	private String email;
+	private String phone;
+
+	public MemberDTO() {
+	}
+
+	public MemberDTO(String id, String pwd, String name, String email, String phone,int user_code) {
+		this.id = id;
+		this.pwd = pwd;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.user_code = user_code;
+
+
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String email) {
+		this.phone = phone;
+	}
+
+
+	public int getUser_code() {
+		return user_code;
+	}
+
+	public void setUser_code(int user_code) {
+		this.user_code = user_code;
+	}
+
+	@Override
+	public String toString() {
+		return "MemberDTO{" +
+				"id='" + id + '\'' +
+				", pwd='" + pwd + '\'' +
+				", name='" + name + '\'' +
+				", email='" + email + '\'' +
+				", phone='" + phone + '\'' +
+				", user_code='" + user_code + '\'' +
+				'}';
+	}
+}
