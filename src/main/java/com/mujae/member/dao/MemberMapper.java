6136b474ce81a7ba76aa2f066d4dc314dd7a51dc
zzimmi <miryeong0812@naver.com>
2024-03-18 21:08:54 +0900

    20240318 로그인, 회원가입 기능  추가

--- src/main/java/com/mujae/member/dao/MemberMapper.java
+++ src/main/java/com/mujae/member/dao/MemberMapper.java
@@ -6,27 +6,19 @@ import com.mujae.member.dto.MemberDTO;
 
 @Mapper
 public interface MemberMapper {
-
-	int idCheck(String id); //아이디 체크
-
 	int memberJoin(MemberDTO mdto); //회원가입
 
-//	MemberDTO getMember(MemberDTO mdto); //회원정보 가져오기
-
-
-
-	String searchId(MemberDTO mdto); //아이디 찾기
-
-	int updatePasswd(MemberDTO mdto); //비밀번호 수정
+	MemberDTO getMember(MemberDTO mdto); //회원정보 가져오기
 
 	int memUpProc(MemberDTO mdto); //회원정보 수정
 
 	int memDelete(MemberDTO mdto); //탈퇴하기
 
-
-//	MemberDTO getMember(MemberDTO mdto);
-
     MemberDTO selectUser();
 
-	MemberDTO getMember(String id, String pwd);
+	MemberDTO findByEmail(String email); //이메일 중복
+	MemberDTO findByUserId(String id); //아이디 중복
+
+	MemberDTO findByPhone(String phone); //전화번호 중복
+
 }
--- src/main/java/com/mujae/member/service/MemberService.java
+++ src/main/java/com/mujae/member/service/MemberService.java
@@ -5,15 +5,9 @@ import com.mujae.member.dto.MemberDTO;
 
 public interface MemberService {
 
-	int idCheck(String id); //아이디 체크
-
 	int memberJoin(MemberDTO mdto) throws Exception; //회원가입
 
 	MemberDTO getMember(MemberDTO mdto); //회원정보 가져오기
-
-	String searchId(MemberDTO mdto); //아이디 찾기
-
-	int updatePasswd(MemberDTO mdto); //비밀번호 수정
 
 	int memUpProc(MemberDTO mdto); //회원정보 수정
 
